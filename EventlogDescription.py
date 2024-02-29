3a02169870b50c1060d503862e490510ca94dfb5
Toontelen <toon.telen@gmail.com>
2024-02-29 14:51:56 +0100

    added avg length

--- EventlogDescription.py
+++ EventlogDescription.py
@@ -9,12 +9,18 @@ def log_statistics(event_log):
     end=pm4py.get_end_activities(event_log)
     max_length = max(len(key) for key in variants.keys())
     min_length=min(len(key) for key in variants.keys())
+    avg_length = round(np.mean([len(key) for key in variants.keys()]))
+    max_duration= max(pm4py.stats.get_case_duration(str(case)) for case in range(0, len(event_log)))
+
+
     
     data = {
         'Start Activities': list(start),
         'End Activities': list(end),
         'Max Length': [max_length],
-        'Min Length': [min_length]
+        'Min Length': [min_length],
+        'Avg Length': [avg_length],
+        'max_duration':[max_duration]
     }
 
     print(data)
