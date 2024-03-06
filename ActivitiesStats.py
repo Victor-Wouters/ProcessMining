d2299893aa7f6ce498327ec4be1b8973bc3cbb36
Toontelen <toon.telen@gmail.com>
2024-03-06 16:10:02 +0100

    Update ActivitiesStats.py

--- ActivitiesStats.py
+++ ActivitiesStats.py
@@ -4,6 +4,7 @@ import seaborn as sns
 def activities(eventlog):
     eventlog.groupby('Activity').size().plot(kind='barh', color=sns.palettes.mpl_palette('Dark2'))
     plt.gca().spines[['top', 'right',]].set_visible(False)
+    plt.title("Frequency for each activity")
     plt.show()
     num_events=len(eventlog)
     num_cases = len(eventlog.case_id.unique())
